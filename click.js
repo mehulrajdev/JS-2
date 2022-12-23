1621a984f0dbad0ea07a61848404f1059b9b8e9e
mehulrajdev <mehulrajpnbe@gmail.com>
2022-12-23 20:52:51 +0530

    modified index.html

---
+++ click.js
@@ -0,0 +1,11 @@
+let paragraphElement = document.querySelector('p');
+
+paragraphElement.addEventListener();
+
+// video library and learn journey
+// visualization
+// different language
+// practice the tests
+
+
+//intro , rapo building ,need creation , coucelling session , booking appoint ,closing the call
---
+++ exercise.js
@@ -0,0 +1,17 @@
+let  h1Element = document.body.firstElementChild;
+h1Element = document.body.children[0];
+console.dir(h1Element);
+
+
+console.dir(h1Element.parentElement);
+
+console.dir(h1Element.nextElementSibling);
+
+h1Element = document.getElementById('first-title');
+console.dir(h1Element);
+
+
+let paragraph =document.querySelector('.paragraph');
+console.dir(paragraph);
+
+paragraph.textContent='hello mehul';
