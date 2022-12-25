69fe47c8dbd8ab220b1ca32fbfa4698210aeda1e
mehulrajdev <mehulrajpnbe@gmail.com>
2022-12-25 11:26:42 +0530

    modified exercise.js

--- click.js
+++ click.js
@@ -1,5 +1,20 @@
 let paragraphElement = document.querySelector('p');
 
-paragraphElement.addEventListener();
+
+function  changeParagraph(){
+    paragraphElement.textContent='Clicked !';
+}
+
+paragraphElement.addEventListener('click',changeParagraph);
+
+let inputElement =document.querySelector('input');
+
+function  retrieveUser(){
+    let enteredText =inputElement.value;
+    console.log(enteredText);
+}
+
+inputElement.addEventListener('input',retrieveUser);
+
 
 
--- exercise.js
+++ exercise.js
@@ -16,11 +16,3 @@ console.dir(paragraph);
 
 paragraph.textContent='hello mehul';
 
-// video library and learn journey
-// visualization
-// different language
-// practice the tests
-
-
-
-//intro , rapo building ,need creation , coucelling session , booking appoint ,closing the call
