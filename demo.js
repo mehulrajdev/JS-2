da7c2e6559452c706c0fe54a561e17bbd9a0f790
mehulrajdev <mehulrajpnbe@gmail.com>
2022-12-26 11:03:45 +0530

    modified demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,14 @@
 let productName =document.getElementById('product-name');
+let remainingChars = document.getElementById('remaining-chars');
+
+let maxAllowed = productName.maxLength;
+
+function updateRemaining(event){
+    let enteredText =event.target.value;
+    let enteredTextLength = enteredText.length;
+
+    let remainingCharacter = maxAllowed - enteredTextLength;
+    remainingChars.textContent =remainingCharacter;
+}
+
+productName.addEventListener('input',updateRemaining);
