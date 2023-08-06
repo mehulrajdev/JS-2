7f7822dbfbde905e446cfda73c9ed948bebf2af1
Mehul <82897404+mehulrajdev@users.noreply.github.com>
2023-08-07 02:21:34 +0530

    Practice

---
+++ assg_1/index.js
@@ -0,0 +1,15 @@
+var name = "Akan";
+var age = 20;
+var basicsal = 200000;
+
+var totalsal, hra, pf;
+pf = (12 / 100) * basicsal;
+hra = (24 / 100) * basicsal;
+
+totalsal = basicsal + hra + pf;
+
+console.log("username is " + name);
+console.log("basic salary is " + basicsal);
+console.log("pf is " + pf);
+console.log("hra is " + hra);
+console.log("total salary is " + totalsal);
